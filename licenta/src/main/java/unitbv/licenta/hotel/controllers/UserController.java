3ff64eb14d6b97bc383c55eb7e0bae220266d44c
Linda Szasz <lindas_bv@yahoo.com>
2018-06-19 20:08:24 +0300

    Implemented account info page.

--- licenta/src/main/java/unitbv/licenta/hotel/controllers/UserController.java
+++ licenta/src/main/java/unitbv/licenta/hotel/controllers/UserController.java
@@ -1,6 +1,8 @@
 package unitbv.licenta.hotel.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +12,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import unitbv.licenta.hotel.models.User;
 import unitbv.licenta.hotel.repositories.UserRepository;
+import unitbv.licenta.hotel.service.UserService;
 
 @Controller
 @RequestMapping("/clients")
@@ -17,6 +20,9 @@ public class UserController {
 
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private UserService userService;
 
 	@RequestMapping
 	public ModelAndView getUsers(@RequestParam(required = false) String name) {
@@ -28,8 +34,8 @@ public class UserController {
 			users = userRepository.findAll();
 		}
 
-		return new ModelAndView("admin/clients", "users", users);	
-		
+		return new ModelAndView("admin/clients", "users", users);
+
 	}
 
 	@RequestMapping(path = "show", method = RequestMethod.GET)
@@ -37,11 +43,19 @@ public class UserController {
 		User user = id != null ? userRepository.findOne(id) : new User();
 		return new ModelAndView("admin/client", "user", user);
 	}
-	
-	@RequestMapping(path = "showForHeader", method = RequestMethod.GET)
-	public ModelAndView getUserForHeader(@RequestParam(value = "id", required = false) Long id) {
-		User user = userRepository.findOne(id);
-		return new ModelAndView("admin/headerAdmin", "user", user);
+
+	@RequestMapping(path = "/account", method = RequestMethod.GET)
+	public ModelAndView getUserForHeader() {
+
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		User user = userService.findUserByEmail(auth.getName());
+
+		User myUser = userRepository.findOne(user.getId());
+		if(myUser.getActive() == 2)
+		   return new ModelAndView("account-info", "user", myUser);
+		else
+		   return new ModelAndView("admin/account-info", "user", myUser);
+
 	}
 
 	@RequestMapping(path = "/add", method = RequestMethod.GET)
@@ -66,9 +80,8 @@ public class UserController {
 	@RequestMapping(path = "/update", method = RequestMethod.GET)
 	public String updateUser(@RequestParam(value = "id") long id, @RequestParam(value = "firstName") String firstName,
 			@RequestParam(value = "lastName") String lastName, @RequestParam(value = "cnp") String cnp,
-			@RequestParam(value = "phone") String phone, @RequestParam(value = "email") String email
-			) {
-		//@RequestParam(value = "password", required=false) String password
+			@RequestParam(value = "phone") String phone, @RequestParam(value = "email") String email) {
+	
 		User user = userRepository.findOne(id);
 
 		user.setFirstName(firstName);
@@ -76,10 +89,30 @@ public class UserController {
 		user.setCnp(cnp);
 		user.setPhone(phone);
 		user.setEmail(email);
-		//user.setPassword(password);
 
 		userRepository.save(user);
 		return "redirect:/clients";
+		// return "Client was updated";
+
+	}
+	
+	@RequestMapping(path = "/update/account", method = RequestMethod.GET)
+	public String updateAccountInfo(@RequestParam(value = "id") long id, @RequestParam(value = "firstName") String firstName,
+			@RequestParam(value = "lastName") String lastName, @RequestParam(value = "cnp") String cnp,
+			@RequestParam(value = "phone") String phone, @RequestParam(value = "email") String email,
+			@RequestParam(value = "password", required=false) String password) {
+		
+		User user = userRepository.findOne(id);
+
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+		user.setCnp(cnp);
+		user.setPhone(phone);
+		user.setEmail(email);
+		user.setPassword(password);
+
+		userRepository.save(user);
+		return "redirect:/home";
 		// return "Client was updated";
 
 	}
